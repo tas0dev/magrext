94e191c467902dd43d4251dc9cf363179cff92a3
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2026-02-14 11:52:30 +0900

    lib.rsに各種モジュールを定義

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// modの実装などを読み込む
+pub mod mods;
+/// modのメタデータなどを読み込む
+pub mod metadata;
+/// modを取得したり、modを管理するための構造体などを定義
+pub mod nest;
+/// modを実行するためのランタイムなどを定義
+pub mod runtime;
+/// modを開発するためのSDKなどを定義
+pub mod sdk;
